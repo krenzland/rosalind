d442c65bc38c72985f31ca865873e81922504ab7
Lukas Krenz <lukas@krenz.land>
2015-02-23 13:50:17 +0100

    instead of 	(...yeah...)

--- gc/src/main.rs
+++ gc/src/main.rs
@@ -15,12 +15,12 @@ use std::old_io::File;
 
 #[derive(Debug)]
 struct Fasta {
-	label: String,
-	content: String,
+    label: String,
+    content: String,
 }
 
 fn main () {
-  	let args: Vec<String> = env::args().map(|x| x.to_string())
+    let args: Vec<String> = env::args().map(|x| x.to_string())
                                        .collect();
 
     let path = match args.as_slice() {
@@ -29,67 +29,67 @@ fn main () {
 
     };
 
-	let mut file = match File::open(&path) {
-		Err(why) => panic!("couldn't open {}: {}", path.display(), why.desc),
-		Ok(file) => file,
-	};
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", path.display(), why.desc),
+        Ok(file) => file,
+    };
 
-	let content = match file.read_to_string() {
-		Err(why) => panic!("couldn't read {}: {}", path.display(), why.desc),
-		Ok(string) => string, 
-	};
+    let content = match file.read_to_string() {
+        Err(why) => panic!("couldn't read {}: {}", path.display(), why.desc),
+        Ok(string) => string, 
+    };
 
-	let input: &str = content.as_slice();
+    let input: &str = content.as_slice();
 
-	let dnas = parse_fasta(input);
+    let dnas = parse_fasta(input);
 
-	let mut max_name = "".to_string();
-	let mut max_gc_content = 0.0;
+    let mut max_name = "".to_string();
+    let mut max_gc_content = 0.0;
 
-	for dna in dnas {
-		let gc_content = get_gc_content(&dna);
-		if (max_gc_content <= gc_content) {
-			max_name = dna.label;
-			max_gc_content = gc_content;
-		}
-	}
+    for dna in dnas {
+        let gc_content = get_gc_content(&dna);
+        if (max_gc_content <= gc_content) {
+            max_name = dna.label;
+            max_gc_content = gc_content;
+        }
+    }
 
-	println!("{}\n{}", max_name, max_gc_content);
+    println!("{}\n{}", max_name, max_gc_content);
 
 }
 
 fn get_gc_content(dna: &Fasta) -> f64 {
-	let mut counter = 0;
-	let size = dna.content.len();
+    let mut counter = 0;
+    let size = dna.content.len();
 
-	for c in dna.content.graphemes(true) {
-		if c == "C" || c == "G" {
-			counter += 1;
-		}
-	}
-	(100 * counter) as f64/size as f64
+    for c in dna.content.graphemes(true) {
+        if c == "C" || c == "G" {
+            counter += 1;
+        }
+    }
+    (100 * counter) as f64/size as f64
 }
 
 fn parse_fasta(input: &str) -> Vec<Fasta> {
-	let mut result: Vec<Fasta> = Vec::new();
-	let codes: Vec<&str> = input.split('>').collect();
+    let mut result: Vec<Fasta> = Vec::new();
+    let codes: Vec<&str> = input.split('>').collect();
 
-	for c in codes {
-		let tokens: Vec<&str> = c.splitn(1, '\n').collect();
-		if tokens.len() == 2 {
-			let res = vec_to_fasta(tokens);
-			result.push(res);
-		}
-	}
+    for c in codes {
+        let tokens: Vec<&str> = c.splitn(1, '\n').collect();
+        if tokens.len() == 2 {
+            let res = vec_to_fasta(tokens);
+            result.push(res);
+        }
+    }
 
-	result
+    result
 }
 
 
 fn vec_to_fasta(vec: Vec<&str>) -> Fasta {
-	let label = vec[0].to_string();
-	let content = vec[1].replace("\n","");
-	Fasta {label: label, 
-			content: content}
-	}
+    let label = vec[0].to_string();
+    let content = vec[1].replace("\n","");
+    Fasta {label: label, 
+            content: content}
+    }
 
